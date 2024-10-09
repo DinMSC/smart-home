3f4945cd08318dbd562174bb85104a3a2d8bf0ef
DinDevCons <din@devcons.com>
2024-10-09 14:31:24 +0200

    added effects

--- fronted/src/components/PageHeader.tsx
+++ fronted/src/components/PageHeader.tsx
@@ -1,3 +1,9 @@
+const MenuItem = ({ text }: { text: string }) => (
+  <div className='cursor-pointer h-[70px] w-[70px] pt-[5px] flex flex-row justify-center items-center border-b-[5px] border-transparent hover:border-[#FF4D00] hover:text-[#FF4D00]'>
+    <li>{text}</li>
+  </div>
+);
+
 const PageHeader = () => {
   return (
     <div className='flex flex-row w-full gap-4 bg-black text-white font-semibold'>
@@ -5,15 +11,19 @@ const PageHeader = () => {
       <div className='h-[70px] w-full flex justify-center items-center'>
         <ul
           id='menu'
-          className='hidden fixed top-0 right-0 px-10 py-16 z-50
-              md:relative md:flex md:p-0 md:bg-transparent md:flex-row md:space-x-6 cursor-pointer'
+          className='hidden fixed top-0 right-0 px-10 py-16 z-50 h-full justify-center items-center
+              md:relative md:flex md:p-0 md:bg-transparent md:flex-row md:space-x-6'
         >
-          <li>Home</li>
-          <li>Contact</li>
-          <li>About</li>
+          <MenuItem text='Home' />
+          <MenuItem text='Contact' />
+          <MenuItem text='About us' />
         </ul>
       </div>
-      <div className='flex flex-row items-center pr-8'>BUTTON</div>
+      <div className='flex flex-row justify-center items-center pr-8'>
+        <button className='flex flex-row justify-center items-center w-[169px] h-[38px] bg-[#FF4D00] rounded-md px-2 py-1'>
+          Schedule a demo
+        </button>
+      </div>
     </div>
   );
 };
